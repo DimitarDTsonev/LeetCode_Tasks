99522fb9e230a140b6ba1c318f995f48cbe5c13c
Димитър Цонев <mitkoconev63@gmail.com>
2023-11-17 01:22:56 +0200

    Added task 00714

---
+++ 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+
+int maxProfit(std::vector<int>& prices, int fee) {
+	int buy = INT_MIN, sell = 0;
+
+	for (int price : prices) {
+		buy = std::max(buy, sell - price);
+		sell = std::max(sell, buy + price - fee);
+	}
+
+	return sell;
+}
+
+int main() {
+	std::vector<int> v{ 1, 3, 2, 8, 4, 9 };
+	int fee = 2;
+
+	std::cout << maxProfit(v, fee);
+
+	return 0;
+}
