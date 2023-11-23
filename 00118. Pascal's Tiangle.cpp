7147017fb8f8a827ebfc32d23872801e621ef215
Димитър Цонев <mitkoconev63@gmail.com>
2023-11-23 17:17:30 +0200

    Added 00118.

---
+++ 00118. Pascal's Tiangle.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+
+void print(std::vector<std::vector<int>> vec){
+    unsigned length = vec.size();
+    for (size_t i = 0; i < length; i++) {
+        unsigned lengthi = vec[i].size();
+        for (size_t j = 0; j < lengthi; j++) {
+            std::cout << vec[i][j] << " ";
+        }
+        std::cout << std::endl;        
+    }
+}
+
+std::vector<std::vector<int>> generate(int numRows) {
+    std::vector<std::vector<int>> triangle;
+
+    for (int i = 0; i < numRows; ++i) {
+        std::vector<int> row(i + 1, 1);
+        for (int j = 1; j < i; ++j) {
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+
+        triangle.push_back(row);
+    }
+
+    return triangle;
+}
+
+int main(){
+    int numRows = 5;
+    std::vector<std::vector<int>> toReturn;
+
+    //std::cin >> numRows;
+    toReturn = generate(numRows);
+    print(toReturn);
+
+    return 0;
+}
