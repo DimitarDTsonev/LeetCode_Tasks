55a145cff3c2c9df222e4bceb195435cf499d8c5
mitko430 <mitkoconev63@gmail.com>
2023-12-05 22:59:30 +0200

    Added task 0003

---
+++ 0003. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+
+int lengthOfLongestSubstring(std::string s) {
+        int n = s.length();
+        int maxLength = 0;
+        std::unordered_map<char, int> charMap;
+        int left = 0;
+        
+        for (int right = 0; right < n; right++) {
+            if (charMap.count(s[right]) == 0 || charMap[s[right]] < left) {
+                charMap[s[right]] = right;
+                maxLength = std::max(maxLength, right - left + 1);
+            } else {
+                left = charMap[s[right]] + 1;
+                charMap[s[right]] = right;
+            }
+        }
+        return maxLength;
+    }
+
+int main(){
+    std::string s = "abcabc";
+    std::string s1 = "bbbbb";
+    std::string s2 = "pwwkew";
+    
+    std::cout << lengthOfLongestSubstring(s) << std::endl;
+    std::cout << lengthOfLongestSubstring(s1) << std::endl;
+    std::cout << lengthOfLongestSubstring(s2) << std::endl;
+
+    return 0;
+}
