86ea9a98c8f62b0b4db5b7cd064e00489101e3de
Димитър Цонев <56803540+mitko430@users.noreply.github.com>
2023-12-06 02:33:21 +0200

    Create 0938. Range Sum of BST.cpp

---
+++ 0938. Range Sum of BST.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+int rangeSumBST(TreeNode* root, int low, int high) {
+	if (!root || low > high) {
+		return 0;
+	}
+
+	if (root->val < low) {
+		return rangeSumBST(root->right, low, high);
+	}
+	if (high < root->val) {
+		return rangeSumBST(root->left, low, high);
+	}
+	return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+}
+
+int main() {
+	// Test:
+	TreeNode* root = new TreeNode(10,
+		new TreeNode(5,
+			new TreeNode(3,
+				new TreeNode(1),
+				nullptr
+			),
+			new TreeNode(7,
+				nullptr,
+				new TreeNode(6)
+			)
+		),
+		new TreeNode(15,
+			new TreeNode(13),
+			new TreeNode(18)
+		)
+	);
+
+
+	std::cout << rangeSumBST(root, 6, 10);
+
+	delete root;
+	root = nullptr;
+	
+	return 0;
+}
