9d7efbf00d2ee75f725e82942fa27bd4aba92b12
Dimitar Tsonev <56803540+mitko430@users.noreply.github.com>
2024-01-15 22:53:23 +0200

    Update 0098. Validate Binary Search Tree.cpp

--- 0098. Validate Binary Search Tree.cpp
+++ 0098. Validate Binary Search Tree.cpp
@@ -21,3 +21,23 @@ void deleteTree(TreeNode* tree) {
 	
 	delete tree;
 }
+
+bool validate(TreeNode* root, long min, long max) {
+	if (!root) {
+		return true;
+	}
+
+	if (root->val <= min || root->val >= max) {
+		return false;
+	}
+
+	return validate(root->left, min, root->val) &&
+		validate(root->right, root->val, max);
+}
+
+bool isValidBST(TreeNode* root) {
+	if (!root) {
+		return true;
+	}
+	return validate(root, LONG_MIN, LONG_MAX);
+}
