ab4e0e8df747c9bf38056d0a2c6d6dbaefdff12d
Dimitar Tsonev <56803540+mitko430@users.noreply.github.com>
2024-01-15 22:52:03 +0200

    Create 0098. Validate Binary Search Tree.cpp

---
+++ 0098. Validate Binary Search Tree.cpp
@@ -0,0 +1,13 @@
+// Task: https://leetcode.com/problems/validate-binary-search-tree/description/
+
+#include<iostream>
+#include<vector>
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
