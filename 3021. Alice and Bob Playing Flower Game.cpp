9b74ed42efa49a7ae24e327f0dede1f19f764b67
Dimitar Tsonev <56803540+mitko430@users.noreply.github.com>
2024-02-03 16:26:39 +0200

    Update 3021. Alice and Bob Playing Flower Game.cpp

--- 3021. Alice and Bob Playing Flower Game.cpp
+++ 3021. Alice and Bob Playing Flower Game.cpp
@@ -1,3 +1,17 @@
 // Task: https://leetcode.com/problems/alice-and-bob-playing-flower-game/description/
 
 #include<iostream>
+
+long long flowerGame(int n, int m) {
+    return (long long) n * m / 2;
+}
+
+int main() {
+    // Example 1:
+    std::cout << flowerGame(3, 2);
+
+    // Example 2:
+    std::cout << flowerGame(1, 1);
+    
+    return 0;
+}
