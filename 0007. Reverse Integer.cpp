d0cd19b43f5d3a8f3fe01a1122b702637c5a9356
Dimitar Tsonev <56803540+mitko430@users.noreply.github.com>
2024-02-03 02:18:30 +0200

    Update 0007. Reverse Integer.cpp

--- 0007. Reverse Integer.cpp
+++ 0007. Reverse Integer.cpp
@@ -1,1 +1,32 @@
+// Task: https://leetcode.com/problems/reverse-integer/description/
 
+int reverse(int x) {
+    long long res = 0;
+    long long xCopy = x;
+
+    while (xCopy) {
+        res = res * 10 + (xCopy % 10);
+        xCopy /= 10;
+    }
+
+    if ((res > 2147483647 && x >= 0) || (res <= -2147483648 && 0 > x)) {
+        return 0;
+    }
+
+    return res;
+}
+
+int main() {
+    // Example 1:
+    std::cout << reverse(123) << std::endl;
+    
+    // Example 2:
+    std::cout << reverse(-123) << std::endl;
+    // Example 3:
+    std::cout << reverse(120) << std::endl;
+
+	// Example 4:
+	std::cout << reverse(-1534236469) << std::endl;
+
+	return 0;
+}
