b5d926ff9f9e7dfee750ad8e2db81c0cbdb0fe9e
Dimitar Tsonev <56803540+mitko430@users.noreply.github.com>
2024-02-04 06:51:54 +0200

    Update 2027. Minimum Moves to Convert String.cpp

--- 2027. Minimum Moves to Convert String.cpp
+++ 2027. Minimum Moves to Convert String.cpp
@@ -1,1 +1,37 @@
+// Task: https://leetcode.com/problems/minimum-moves-to-convert-string/
 
+#include<iostream>
+#include<string>
+
+int minimumMoves(std::string s) {
+	int i = 0;
+	int size = s.size();
+	unsigned counter = 0;
+
+	while (i < size) {
+		if (s[i] == 'O') {
+			++i;
+		}
+		else {
+			++counter;
+			i += 3;
+		}
+	}
+	return counter;
+}
+
+int main() {
+	// Example 1:
+	std::cout << minimumMoves("XXXXX") << std::endl;
+
+	// Example 2:
+	std::cout << minimumMoves("OOOX") << std::endl;     
+
+	//Example 3:
+	std::cout << minimumMoves("XOOO") << std::endl;
+
+	// Example 4:
+	std::cout << minimumMoves("XXOX") << std::endl;     
+
+	return 0;
+}
