634c48e697a3cc867376c599381522527330cb10
Dimitar Tsonev <56803540+mitko430@users.noreply.github.com>
2024-02-29 14:49:21 +0200

    Update 0933. Number of Recent Calls.cpp

--- 0933. Number of Recent Calls.cpp
+++ 0933. Number of Recent Calls.cpp
@@ -22,10 +22,10 @@ public:
 int main() {
   // Example 1:
   RecentCounter recentCounter = new RecentCounter();
-  std::cout << recentCounter.ping(1) << " ";     // requests = [1], range is [-2999,1], return 1
-  std::cout << recentCounter.ping(100) << " ";   // requests = [1, 100], range is [-2900,100], return 2
-  std::cout << recentCounter.ping(3001) << " ";  // requests = [1, 100, 3001], range is [1,3001], return 3
-  std::cout << recentCounter.ping(3002) << " ";  // requests = [1, 100, 3001, 3002], range is [2,3002], return 3
+  std::cout << recentCounter.ping(1) << " ";
+  std::cout << recentCounter.ping(100) << " ";
+  std::cout << recentCounter.ping(3001) << " ";
+  std::cout << recentCounter.ping(3002) << " ";
   
   return 0;
 }
