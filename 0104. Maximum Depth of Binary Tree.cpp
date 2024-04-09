4210e025c603f12486c0da35c51820ae0905d974
Dimitar Tsonev <56803540+mitko430@users.noreply.github.com>
2024-04-09 20:40:15 +0300

    Update 0104. Maximum Depth of Binary Tree.cpp

--- 0104. Maximum Depth of Binary Tree.cpp
+++ 0104. Maximum Depth of Binary Tree.cpp
@@ -1,4 +1,14 @@
-// Task : 
+// Task : https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr){}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode* _left, TreeNode* right) : val(x), left(_left), right(right){}
+};
 
 int maxDepth(TreeNode* root) {
         if(!root){
@@ -8,6 +18,7 @@ int maxDepth(TreeNode* root) {
 }
 
 int main() { 
+    // Example 1:
     
     return 0;
 }
